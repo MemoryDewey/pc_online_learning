750fb5e5ea2e7ae5059d0c14b6948374e6803ed1
MemoryDewey <memorydewey@vip.qq.com>
2019-08-15 09:20:06 +0800

    暂时关闭没有用到的功能

--- src/components/profile/Certificate.vue
+++ src/components/profile/Certificate.vue
@@ -1,5 +1,26 @@
 <template>
+    <div class="flex-list">
+        <div class="flex-list-header">
+            <div class="flex-row">
+                <div class="flex-cell first">课程</div>
+                <div class="flex-cell">证书编号</div>
+                <div class="flex-cell">操作</div>
+            </div>
+        </div>
+        <div class="flex-list-no-data" v-if="true">
+            <div class="status-box">
+                <div class="box-icon">
+                    <span>
+                        <font-awesome-icon icon="info-circle"></font-awesome-icon>
+                    </span>
+                </div>
+                <div class="box-text">
+                    <div>暂无证书信息</div>
+                </div>
+            </div>
+        </div>
 
+    </div>
 </template>
 
 <script>
