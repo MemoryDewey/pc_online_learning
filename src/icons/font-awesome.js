afb1b7ccc1d9a628436f38c2b1439ebd6fee4a2c
MemoryDewey <memorydewey@vip.qq.com>
2019-09-02 12:13:39 +0800

    更新引入FontAwesome时独立为一个文件

---
+++ src/icons/font-awesome.js
@@ -0,0 +1,25 @@
+/* font-awesome 图标库*/
+import {library} from '@fortawesome/fontawesome-svg-core'
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
+import {
+    faChevronLeft, faChevronRight, faSearch, faWallet, faCog, faSignOutAlt,
+    faAddressCard, faBookOpen, faFileAlt, faTasks, faAward, faEnvelope,
+    faInfoCircle, faAngleRight, faLongArrowAltDown, faThLarge, faBook,
+    faPlayCircle, faVideo, faCalendarAlt, faFileImage, faFileArchive,
+    faFilePdf, faFilePowerpoint, faFileWord, faFileExcel, faStar, faCheck,
+    faTimes, faPlus, faCheckCircle, faShieldAlt, faMobileAlt, faTimesCircle,
+    faUser, faLock, faExchangeAlt, faAngleDoubleUp, faAngleDoubleDown,
+    faExclamationCircle, faYenSign, faShareAlt, faShoppingCart
+} from '@fortawesome/free-solid-svg-icons'
+
+library.add(
+    faChevronRight, faChevronLeft, faSearch, faWallet, faCog, faSignOutAlt,
+    faAddressCard, faBookOpen, faFileAlt, faTasks, faAward, faEnvelope,
+    faInfoCircle, faAngleRight, faLongArrowAltDown, faThLarge, faBook,
+    faPlayCircle, faVideo, faCalendarAlt, faFileImage, faFileArchive,
+    faFilePdf, faFilePowerpoint, faFileWord, faFileExcel, faStar, faCheck,
+    faTimes, faPlus, faCheckCircle, faShieldAlt, faMobileAlt, faTimesCircle,
+    faUser, faLock, faExchangeAlt, faAngleDoubleDown, faAngleDoubleUp,
+    faExclamationCircle, faYenSign, faShareAlt, faShoppingCart
+);
+export default FontAwesomeIcon
--- src/main.js
+++ src/main.js
@@ -7,32 +7,9 @@ import ELEMENT from 'element-ui'
 import VueClipboard from 'vue-clipboard2'
 // import 'element-ui/lib/theme-chalk/index.css'
 import VueLazyLoad from 'vue-lazyload'
-/* font-awesome 图标库 S*/
-import {library} from '@fortawesome/fontawesome-svg-core'
-import {
-    faChevronLeft, faChevronRight, faSearch, faWallet, faCog, faSignOutAlt,
-    faAddressCard, faBookOpen, faFileAlt, faTasks, faAward, faEnvelope,
-    faInfoCircle, faAngleRight, faLongArrowAltDown, faThLarge, faBook,
-    faPlayCircle, faVideo, faCalendarAlt, faFileImage, faFileArchive,
-    faFilePdf, faFilePowerpoint, faFileWord, faFileExcel, faStar, faCheck,
-    faTimes, faPlus, faCheckCircle, faShieldAlt, faMobileAlt, faTimesCircle,
-    faUser, faLock, faExchangeAlt, faAngleDoubleUp, faAngleDoubleDown,
-    faExclamationCircle, faYenSign, faShareAlt
-} from '@fortawesome/free-solid-svg-icons'
+import FontAwesomeIcon from "./icons/font-awesome";
 
-import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
-/* font-awesome 图标库 E*/
-library.add(
-    faChevronRight, faChevronLeft, faSearch, faWallet, faCog, faSignOutAlt,
-    faAddressCard, faBookOpen, faFileAlt, faTasks, faAward, faEnvelope,
-    faInfoCircle, faAngleRight, faLongArrowAltDown, faThLarge, faBook,
-    faPlayCircle, faVideo, faCalendarAlt, faFileImage, faFileArchive,
-    faFilePdf, faFilePowerpoint, faFileWord, faFileExcel, faStar, faCheck,
-    faTimes, faPlus, faCheckCircle, faShieldAlt, faMobileAlt, faTimesCircle,
-    faUser, faLock, faExchangeAlt, faAngleDoubleDown, faAngleDoubleUp,
-    faExclamationCircle, faYenSign, faShareAlt
-);
-Vue.component('font-awesome-icon', FontAwesomeIcon);
+Vue.component('font-awesome-icon',FontAwesomeIcon);
 VueClipboard.config.autoSetContainer = true;
 Vue.use(VueClipboard);
 Vue.use(VueLazyLoad, {
