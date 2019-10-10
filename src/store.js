205c5eaa7650fed8084f1b62156bb55a1f95b78c
Dewey <38499618+MemoryDewey@users.noreply.github.com>
2019-10-10 18:47:43 +0800

    删除前端BST支付功能

--- src/store.js
+++ src/store.js
@@ -1,7 +1,5 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import getWeb3 from '@/utils/getWeb3'
-import getContract from '@/utils/getContract'
 
 Vue.use(Vuex);
 
@@ -11,16 +9,7 @@ export default new Vuex.Store({
         loginState: false,
         avatarUrl: '',
         nickname: '',
-        isTeacher: false,
-        web3: {
-            isInjected: false,
-            web3Instance: null,
-            networkId: null,
-            coinbase: null,
-            balance: null,
-            error: null
-        },
-        contractInstance: null,
+        isTeacher: false
     },
     mutations: {
         login: (state, payload) => {
@@ -36,43 +25,6 @@ export default new Vuex.Store({
         },
         changeNickname: (state, payload) => {
             state.nickname = payload;
-        },
-        registerWeb3Instance(state, payload) {
-            //console.log('commited result to registerWeb3Instance mutation');
-            let result = payload;
-            let web3Copy = state.web3;
-            web3Copy.coinbase = result.coinbase;
-            web3Copy.networkId = result.networkId;
-            web3Copy.isInjected = result.injectedWeb3;
-            web3Copy.web3Instance = result.web3;
-            state.web3 = web3Copy;
-        },
-        pollWeb3Instance(state, payload) {
-            //console.log('commited result to pollWeb3Instance mutation, coinbase: ' + payload.coinbase);
-            state.web3.coinbase = payload.coinbase;
-        },
-        registerContractInstance(state, payload) {
-            //console.log('Casino contract instance: ', payload);
-            state.contractInstance = () => payload[0]
-        },
-    },
-    actions: {
-        registerWeb3({commit}) {
-            //console.log('registerWeb3 Action being executed');
-            getWeb3.then(result => {
-                //console.log('committing result to registerWeb3Instance mutation');
-                commit('registerWeb3Instance', result)
-            }).catch(e => {
-                console.log('error in action registerWeb3', e)
-            })
-        },
-        pollWeb3({commit}, payload) {
-            commit('pollWeb3Instance', payload)
-        },
-        getContractInstance({commit}) {
-            getContract.then(result => {
-                commit('registerContractInstance', result)
-            }).catch(e => console.log(e))
-        },
+        }
     }
 })
