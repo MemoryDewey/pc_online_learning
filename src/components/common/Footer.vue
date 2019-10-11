b12361c77d529c8e3e158dcd98fe8dc27edf2d54
MemoryDewey <memorydewey@vip.qq.com>
2019-10-11 16:06:32 +0800

    更新Footer内容

--- src/components/common/Footer.vue
+++ src/components/common/Footer.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="footer-warp">
         <img class="logo" src="../../assets/image/logo-bcs.png" alt="logo">
-        <p>Copyright © 2019 BAAS BlockChainStudy. All Rights Reserved.</p>
+        <p>Copyright © 2019 BaasStudy. All Rights Reserved.</p>
         <p>尝试实业(上海)有限公司 | 东华大学BAAS区块链实验室</p>
         <p>虹口区地址：上海市虹口区四川北路1500号叶大昌大厦307 | 松江区地址：上海市松江区三新北路1800弄21号楼6003--6009</p>
         <p>服务咨询：400-966-0003</p>
